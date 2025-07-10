11d17ac9dddd5fec293a93dbae91d488864e15a1
maiminhduc <81692975+maiminhduc@users.noreply.github.com>
2025-07-10 12:47:13 +0700

    Update Program_Counter.v

--- Program_Counter.v
+++ Program_Counter.v
@@ -1,12 +1,12 @@
 module Program_Counter(
     input logic clk,
-    input logic reset,
+    input logic reset_n,
     input logic [31:0] next_pc,
     output logic [31:0] current_pc
 );
 
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset)
+    always_ff @(posedge clk or negedge reset_n) begin
+        if (!reset_n)
             current_pc <= 32'h00000000;
         else
             current_pc <= next_pc;
