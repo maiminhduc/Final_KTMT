aca0be0aebef6557edd56108e05c18dbedb0c820
maiminhduc <81692975+maiminhduc@users.noreply.github.com>
2025-07-10 12:49:28 +0700

    Update DMEM.v

--- DMEM.v
+++ DMEM.v
@@ -7,18 +7,13 @@ module DMEM(
     output logic [31:0] mem_read_data
 );
 
-    logic [31:0] data_memory [0:255];
+    logic [31:0] memory [0:255];
 
     always_ff @(posedge clk) begin
         if (write_enable)
-            data_memory[mem_addr[9:2]] <= mem_write_data;
+            memory[mem_addr[9:2]] <= mem_write_data;
     end
 
-    always_comb begin
-        if (read_enable)
-            mem_read_data = data_memory[mem_addr[9:2]];
-        else
-            mem_read_data = 32'b0;
-    end
+    assign mem_read_data = read_enable ? memory[mem_addr[9:2]] : 32'b0;
 
 endmodule
